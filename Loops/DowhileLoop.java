7dc6ea34ee76d84ca531c8388de6193c0a16f7d8
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2025-12-26 22:47:57 +0530

    Add files via upload

---
+++ Loops/DowhileLoop.java
@@ -0,0 +1,15 @@
+
+public class DowhileLoop {
+	
+	public static void main(String[] args) {
+		// here we will use do while loop to print numbers from 1 to 10
+		int i = 1; // Initialization
+		System.out.println("The numbers from 1 to 10 are as follows: ");
+		do {
+			System.out.println(i);
+			i++; // Updation
+			// we have to menstion inistiazation, condition and updation in every loop just the way or the syntax to do so is different.
+			}while(i<=10); // Condition
+	}
+
+}
---
+++ Loops/WhileIoop.java
@@ -0,0 +1,21 @@
+
+public class WhileIoop {
+	
+	public static void main(String[] args) {
+		// here we will use the while loop
+		
+		// to print the table of 12 using while loop
+		
+		// in while loop we compulasory need the condition
+		// The while loop and do while loop are quite similar but the 
+		// main difference is that in while loop the condition is checked first and then the loop executes where as in do while the loop executes once.
+		int i = 1; // Initialization
+		System.out.println("The table of twelve is as follows: ");
+		while(i<=10) {
+			// condition and the body.
+			System.out.println(12*i);
+			i++; // Updation
+		}
+	}
+
+}
